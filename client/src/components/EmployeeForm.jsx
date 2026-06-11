beb583fc37f13c571ad09b4b6921102645b053d5
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2026-06-11 12:27:14 +0100

    employee form commit

---
+++ client/src/components/EmployeeForm.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { DEPARTMENTS } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
+
+
+const EmployeeForm = ({initialData, onSuccess, onCancel}) => {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false)
+    /* !!initialData the 2 !! converts string data to boolean */
+    const isEditMode = !!initialData 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+    }
+  return (
+   <form onSubmit={handleSubmit} className="space-y-6 max-w-3xl animate-fade-in">
+    {/* personal information */}
+    <div className="card p-4 sm:p-5">
+        <h3 className="font-medium mb-4 pb-3  border-b border-slate-100">Personal Information</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-base text-slate-700">
+            <div className="">
+                <label className="block mb-2">First Name</label>
+                <input name="firstName" required defaultValue={initialData?.firstName} />
+            </div>
+            <div className="">
+                <label className="block mb-2">Last Name</label>
+                <input name="lastName" required defaultValue={initialData?.lastName} />
+            </div>
+            <div className="">
+                <label className="block mb-2">Phone Number</label>
+                <input name="phone" required defaultValue={initialData?.phone} />
+            </div>
+            <div className="">
+                <label className="block mb-2">Join Date</label>
+                <input type="date" name="joinDate" required defaultValue={initialData?.joinDate ? new Date(initialData.joinDate).toISOString().split('T')[0] : ''} />
+            </div>
+            <div className="sm:col-span-2">
+                <label className="block mb-2">Bio (Optional)</label>
+                <textarea name="bio" required defaultValue={initialData?.bio} rows={3} className="resize-none"  placeholder="Brief description"/>
+            </div>
+        </div>
+    </div>
+    {/* employment details */}
+    <div className="card p-4 sm:p-5">
+        <h3 className="text-base font-medium text-slate-900 mb-4 pb-3 border-b border-slate-100">Employment Details</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-base text-slate-700">
+            <div className="">
+                <label className="block mb-2" >Department</label>
+                <select name="department" defaultValue={initialData?.department || ''}>
+                    <option value="">Select Department</option>
+                    {DEPARTMENTS.map((deptName)=>(
+                        <option key={deptName}  value={deptName}> {deptName}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="">
+                <label className="block mb-2">Position</label>
+                <input name="position" required defaultValue={initialData?.position} />
+            </div>
+            <div className="">
+                <label className="block mb-2">Basic Salary</label>
+                {/* step=0.01 helps you get floating digits */}
+                <input type="number" name="basicSalary" required defaultValue={initialData?.basiceSalary || 0} min='0'  step='0.01' />
+            </div>
+            <div className="">
+                <label className="block mb-2">Allowances</label>
+                <input name="allowances" required defaultValue={initialData?.allowances || 0} type="number"  min='0'  step='0.01'/>
+            </div>
+            <div className="">
+                <label className="block mb-2">Deductions</label>
+                <input name="deductions" required defaultValue={initialData?.deductions || 0} type="number"  min='0'  step='0.01'/>
+            </div>
+            {isEditMode  && (
+                <div className="">
+                <label className="block mb-2">Status</label>
+                <select name="employmentStatus" defaultValue={initialData?.employmentStatus}  >
+                    <option value="ACTIVE">Active</option>
+                    <option value="INACTIVE">Inactive</option>
+                </select>
+            </div>
+            )}
+            
+        </div>
+    </div>
+
+    {/* account set up */}
+     <div className="card p-4 sm:p-5">
+        <h3 className="text-base font-medium mb-4 pb-3 text-slate-900  border-b border-slate-100">Account Setup</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-base text-slate-700">
+            <div className="sm:col-span-2">
+                <label className="block mb-2">Work Email</label>
+                <input name="email" type="email" required defaultValue={initialData?.email} />
+            </div>
+            {!isEditMode && (
+                <div className="">
+                <label className="block mb-2">Temporary Password</label>
+                <input name="password" type="password" required  />
+            </div>
+            )}
+            {isEditMode && (
+                <div className="">
+                <label className="block mb-2">Change Password (Optional)</label>
+                <input name="password" type="password" placeholder="Leave blank to keep current"  />
+            </div>
+            )}
+            <div className="">
+                <label className="block mb-2">System Role</label>
+                <select name="role" defaultValue={initialData?.user?.role || 'EMPLOYEE'} >
+                    <option value="EMPLOYEE">Employee</option>
+                    <option value="ADMIN">Admin</option>
+
+
+                </select>
+            </div>
+        </div>
+    </div>
+    {/* buttons */}
+    <div className="flex flex-col-reverse sm:flex-row justify-end gap-2 pt-2">
+        <button className="btn-secondary" type="button" onClick={()=>(onCancel ? onCancel(): navigate(-1))}>
+           Cancel
+        </button>
+        <button className="btn-primary flex items-center justify-center" disabled={loading} type="button">
+           {loading && <Loader2Icon className="w-5 h-5 mr-2 animate-spin" />}
+           {isEditMode ? 'Update Employee' : 'Create Employee'}
+        </button>
+    </div>
+
+   </form>
+  )
+}
+
+export default EmployeeForm
--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -1,8 +1,112 @@
+import { useCallback, useEffect, useState } from "react"
+import { dummyEmployeeData, DEPARTMENTS } from "../assets/assets";
+import { PlusIcon, SearchIcon, X } from "lucide-react";
+import EmployeeCard from "../components/EmployeeCard";
+import EmployeeForm from "../components/EmployeeForm";
 
 
 const Employees = () => {
+  const [employees, setEmployees]= useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [selectedDept, setSelectedDept] = useState('');
+  const [editEmployee, setEditEmployee] = useState(null);
+  /* we use below state to create newemployee data */
+  const [showCreateModal, setShowCreateModal] = useState(false)
+
+  const fetchEmployees = useCallback(async () => {
+    setLoading(true);
+    setEmployees(dummyEmployeeData.filter((emp)=>(selectedDept? emp.department === selectedDept : emp)));
+    setTimeout(()=>{setLoading(false)}, 1000)
+    
+  }, []);
+
+  useEffect(()=>{
+  fetchEmployees();
+  },[fetchEmployees]);
+
+  const filtered = employees.filter((emp)=>`${emp.firstName} ${emp.lastName} ${emp.position}`.toLowerCase().includes(search.toLowerCase()))
   return (
-    <div>Employees</div>
+    <div className="animate-fade-in">
+      {/* header section */}
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-2 mb-4">
+        <div className="">
+          <h1 className="page-title">Employees</h1>
+          <p className="page-subtitle">Manage your team members</p>
+        </div>
+        <button onClick={()=>setShowCreateModal(true)}
+         className="btn-primary items-center flex gap-1 w-full sm:w-auto justify-center">
+          <PlusIcon size={18}/>Add Employee
+        </button>
+
+      </div>
+      {/* Search bar */}
+      <div className="flex flex-col sm:flex-row gap-2 mb-3">
+        <div className="relative flex-1">
+          <SearchIcon className="absolute left-3.5 top-1/2 transform -translate-y-1/2 text-slate-400 w-4 h-4"/> <input placeholder="Search employee..." className="w-full pl-10!"  onChange={(e)=>setSearch(e.target.value)} value={search}/>
+        </div>
+        <select value={selectedDept} onChange={(e)=>setSelectedDept(e.target.value)} className="max-w-40">
+          <option value="">All Departments</option>
+          {DEPARTMENTS.map((deptName)=>(
+            <option key={deptName} value={deptName}>{deptName}</option>
+          ))}
+        </select>
+      </div>
+      {/* employee cards */}
+      {loading? (
+        <div className="flex justify-center p-8">
+          {/* display loading animation */}
+          <div className="animate-spin h-8 w-8 border-2 border-indigo-600 border-t-transparent rounded-full"/>
+        </div>
+      ) : (
+         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+          {filtered.length === 0 ?(
+            <p className="col-span-full text-center py-8 text-slate-400 bg-white rounded-lg border border-dashed border-slate-200">No employees found</p>
+          ) : (
+            filtered.map((emp)=><EmployeeCard key={emp.id} employee={emp} onDelete={fetchEmployees} onEdit={(e)=>setEditEmployee(e)}/>)
+          ) }
+
+         </div>
+         
+      )}
+
+      {/* Create employee modal */}
+      {showCreateModal && (
+        <div className="fixed bg-black/40 backdrop-blur-sm inset-0 z-50 flex items-start justify-center p-4 overflow-y-auto" onClick={()=>setShowCreateModal(false)}>
+          <div className="">
+            <div className="fixed inset-0"/>
+            <div className="relative bg-white rounded-lg shadow-lg w-full max-w-3xl my-5 animate-fade-in" onClick={(e)=>e.stopPropagation()}>
+              <div className="flex items-center justify-between p-6 pb-0">
+                <div className="">
+                  <h2 className="text-lg font-semibold text-slate-900">Add New Employee</h2>
+                  <p className="text-base text-slate-500 mt-0.5">Create a user account and employee profile</p>
+                </div>
+                <button onClick={()=>setShowCreateModal(false)} className="p-2 rounded-lg hover:bg-slate-100 transition-colors text-slate-400 hover:text-slate-600"> <X className="w-5 h-5"/></button>
+              </div>
+              {/* initialData was taken off as attribute blow bcos its a new emp form */}
+              <div className="p-5"> <EmployeeForm  onSuccess={()=>{setShowCreateModal(false); fetchEmployees();}}  onCancel={()=>setShowCreateModal(false)}/></div>
+            </div>
+          </div>
+        </div>
+      )}
+      {/* Edit employee modal */}
+      {editEmployee && (
+        <div className="fixed inset-0 z-50 items-start flex justify-center p-4 overflow-y-auto bg-black/40 backdrop-blur-sm"  onClick={()=>setEditEmployee(null)}>
+          <div className="relative bg-white rounded-lg shadow-lg w-full max-w-3xl my-4 animate-fade-in"  onClick={(e)=>e.stopPropagation()}>
+            <div className="flex items-center justify-between p-6 pb-0 ">
+              <div className="">
+                  <h2 className="text-lg font-semibold text-slate-900">Edit Employee</h2>
+                  <p className="text-base text-slate-500 mt-0.5">Update employee details</p>
+                </div>
+                <button onClick={()=>setEditEmployee(null)} className="p-2 rounded-lg hover:bg-slate-100 transition-colors text-slate-400 hover:text-slate-600"> <X className="w-5 h-5"/></button>
+            </div>
+            <div className="p-6">
+              <EmployeeForm initialData={editEmployee} onSuccess={()=>{setEditEmployee(null); fetchEmployees();}}  onCancel={()=>setEditEmployee(null)}/>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
