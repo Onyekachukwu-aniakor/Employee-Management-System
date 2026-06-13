08ed065aaabde107eb40701e1369c5e98e0d827f
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2026-06-13 20:07:42 +0100

    applyLeaveModal commit

---
+++ client/src/components/leave/ApplyLeaveModal.jsx
@@ -0,0 +1,94 @@
+import { CalendarDays, FileTextIcon, Loader2Icon, SendIcon, X } from "lucide-react";
+import { useState } from "react"
+
+
+const ApplyLeaveModal = ({onClose, onSuccess, open}) => {
+
+    const [loading, setLoading]= useState(false);
+    const today = new Date();
+    const tomorrow = new Date(today)
+    // then tomorrow's date is below
+    tomorrow.setDate(today.getDate() + 1);
+
+    const minDate = tomorrow.toISOString().split('T')[0]
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+    };
+
+    if(!open){return null}
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/40 backdrop-blur-sm"  onClick={onClose}>
+        <div className="relative bg-white rounded-lg shadow-lg w-full max-w-lg animate-fade-in"
+        Click={(e)=>e.stopPropagation()}>
+            {/* Header */}
+            <div className="flex items-center justify-between p-4 pb-0">
+                <div className="">
+                    <h2 className="text-lg font-bold text-slate-800">Apply for leave</h2>
+                    <p className="text-base text-slate-400 mt-0.5">Submit your leave request for approval</p>
+                </div>
+                <button Click={onClose}
+                 className="p-2 rounded-lg hover:bg-slate-100 transition-colors text-slate-400 hover:text-slate-600">
+                    <X className="w-5 h-5"/>
+                </button>
+            </div>
+
+            {/* Form */}
+            <form onSubmit={handleSubmit} className="p-4 space-y-5">
+                {/* leave type */}
+                <div className="">
+                    <label className="flex items-center gap-2 text-base font-medium text-slate-700 mb-2">
+                        <FileTextIcon className="w-5 h-5 text-slate-400"/>Leave Type
+                        </label>
+                        <select name="type" required>
+                            <option value="SICK">Sick Leave</option>
+                            <option value="CASUAL">Casual Leave</option>
+                            <option value="ANNUAL">Annual Leave</option>
+                        </select>
+                </div>
+                {/* Leave duration */}
+                <div className="">
+                    <label className="flex items-center gap-2 text-base font-medium text-slate-700 mb-2">
+                        <CalendarDays className="w-5 h-5 text-slate-400"/>Duration
+                        </label>
+                        <div className="grid grid-cols-2 gap-3">
+                            <div className="">
+                                <span className="block text-xs text-slate-400 mb-1">From</span>
+                                <input type="date" name="startDate" min={minDate} required />
+                            </div>  
+
+                            <div className="">
+                                <span className="block text-xs text-slate-400 mb-1">To</span>
+                                <input type="date" name="endDate" min={minDate} required />
+                            </div>
+                        </div>     
+                        
+                </div>
+                {/* reason */}
+                <div className="">
+                    <label className="block text-base font-medium text-slate-700 mb-2">
+                        Reason
+                        </label>
+                        <textarea name="reason" required rows={3} className="resize-none" placeholder="Reason for the leave"/>
+                </div>
+                {/* button */}
+                <div className="flex gap-3 pt-2">
+                    <button onClick={onClose} className="btn-secondary flex-1" type='button' >
+                        Cancel
+                    </button>
+
+                    <button onClick={onClose} className="btn-primary flex items-center justify-center gap-2 flex-1" type='submit'  disabled={loading}>
+                        {loading ? <Loader2Icon className="w-5 h-5 animate-spin"/> : <SendIcon className="w-5 h-5 "/>}
+                        {loading? 'Submitting...' : 'Submit'}
+                    </button>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ApplyLeaveModal
--- client/src/pages/Leave.jsx
+++ client/src/pages/Leave.jsx
@@ -1,8 +1,86 @@
+import { useCallback, useEffect, useState} from "react"
+import { dummyLeaveData } from "../assets/assets";
+import Loading from "../components/Loading";
+import { PalmtreeIcon, PlusIcon, ThermometerIcon, UmbrellaIcon } from "lucide-react";
+import LeaveHistory from "../components/leave/LeaveHistory";
+import ApplyLeaveModal from "../components/leave/ApplyLeaveModal";
 
 
 const Leave = () => {
+  const [leaves, setLeaves] = useState([]);
+  const [loading, setLoading]= useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false)
+  const isAdmin = false;
+  const fetchLeaves = useCallback(()=>{
+    setLeaves(dummyLeaveData);
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[]);
+
+  useEffect(()=>{
+    fetchLeaves()
+  },[fetchLeaves]);
+
+  if(loading) return <Loading/>
+// 'l' === leave below
+  const approvedLeaves = leaves.filter((l)=>l.status === 'APPROVED');
+  const sickCount = approvedLeaves.filter((l)=>l.type === 'SICK').length;
+  const casualCount = approvedLeaves.filter((l)=>l.type === 'CASUAL').length;
+  const annualCount = approvedLeaves.filter((l)=>l.type === 'ANNUAL').length;
+
+  const leaveStats = [
+    {
+      label: 'Sick Leave',
+      value:sickCount,
+      icon: ThermometerIcon
+    },
+    {
+      label: 'Casual Leave',
+      value:casualCount,
+      icon: UmbrellaIcon
+    },
+    {
+      label: 'Annual Leave',
+      value:annualCount,
+      icon: PalmtreeIcon
+    },
+  ]
   return (
-    <div>Leave</div>
+    <div className="animate-fade-in">
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-3 mb-4">
+         <div className="">
+          <h2 className="page-title">Leave Management</h2>
+          <p className="page-subtitle">{isAdmin ? 'Manage leave applications' : 'Your leave history and request'}</p>
+        </div>
+        {!isAdmin && !isDeleted && (
+          <button onClick={()=> setShowModal(true)}
+           className="btn-primary flex items-center gap-2 w-full sm:w-auto justify-center"> <PlusIcon className="w-5 h-5"/> Apply for leave</button>
+        )}
+      </div>
+      {!isAdmin && (
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 sm:gap-4 mb-4">
+          {leaveStats.map((s)=>(
+            <div className="card card-hover p-4 sm:p-5  flex items-center gap-3 relative overflow-hidden group" key={s.label}>
+              <div className="absolute top-0 left-0 bottom-0 w-1 rounded-r-full bg-slate-500/70 group-hover:bg-indigo-500/70"/>
+              <div className="bg-slate-100 p-2 rounded-lg group-hover:bg-indigo-50  transition-colors duration-200">
+                <s.icon className="w-5 h-5 text-slate-600 group-hover:text-indigo-600 transition-colors duration-200"/>
+              </div>
+              <div className="">
+                <p className="text-sm font-semibold text-slate-500">{s.label}</p>
+                <p className="text-lg font-bold text-slate-900 tracking-tight">{s.value} <span className="text-sm font-base text-green-800">taken</span></p>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      )}
+      <LeaveHistory  leaves={leaves} isAdmin={isAdmin} onUpdate={fetchLeaves}/>
+      <ApplyLeaveModal open={showModal}  onClose={()=>setShowModal(false)}  onSuccess={fetchLeaves}/>
+       
+        
+    </div>
   )
 }
 
