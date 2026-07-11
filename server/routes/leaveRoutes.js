973a927d690041fb98f8b2b24479311fe86bdf4e
Onyekachukwu Aniakor <carlosaniakorchukwu@gmail.com>
2026-07-11 23:58:51 +0100

    commit

--- server/routes/leaveRoutes.js
+++ server/routes/leaveRoutes.js
@@ -4,8 +4,8 @@ import { createLeave, getLeaves, updateLeaveStatus } from "../controllers/leaveC
 
 const leaveRouter = Router();
 
-leaveRouter.patch('/:id',protect, protectAdmin, updateLeaveStatus)
-leaveRouter.get('/', protect, getLeaves)
-leaveRouter.post('/', protect, createLeave)
+leaveRouter.post("/", protect, createLeave)
+leaveRouter.get("/", protect, getLeaves)
+leaveRouter.patch("/:id", protect, protectAdmin,  updateLeaveStatus)
 
 export default leaveRouter;
